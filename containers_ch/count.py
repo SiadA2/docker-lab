13f96861bcabfbc9d924dda197bdf2cc855561d8
SiadA2 <siadadam2@gmail.com>
2025-09-15 12:22:27 +0100

    add UI

--- containers_ch/count.py
+++ containers_ch/count.py
@@ -1,23 +1,20 @@
 import os
-from flask import Flask, render_template
+from flask import Flask, render_template, render_template_string
 import redis
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates', static_folder='static')
 redis_host = os.getenv('REDIS_HOST', 'redis')
 redis_port = int(os.getenv('REDIS_PORT', 6379))
 r = redis.Redis(host=redis_host, port=redis_port)
 
 @app.route('/')
 def welcome_message():
-    return 'Welcome to the flask visit counter'
-
+    return render_template('index.html')
 
 @app.route('/count')
 def web_count():
     count = r.incr('counter')
-    return f'This page has been visited {count} times'
-
-
+    return render_template('index-count.html', count=count)
 
 
 if __name__ == '__main__':
